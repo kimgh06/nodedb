6a428969c7e189ce1ebe08539f5a6f09cc328c5e
kkh061101 <kkh061101@naver.com>
2023-05-03 23:41:45 +0900

    asdf

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 // eslint-disable-next-line
 import { hydrate, render } from 'react-dom';
+import { ReactDOM } from 'react';
 import App from './App';
 
 const cont = document.getElementById('root');
+const root = ReactDOM.createRoot(cont);
 
 if (cont.hasChildNodes()) {
   hydrate(
@@ -13,7 +15,7 @@ if (cont.hasChildNodes()) {
   );
 }
 else {
-  render(
+  root.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>, cont
