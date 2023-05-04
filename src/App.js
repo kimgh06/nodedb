4790477074ea633a664bda12bdaedab33079b0b4
kkh061101 <kkh061101@naver.com>
2023-05-04 15:27:28 +0900

    insert 가능하게

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes, useParams, useSearchParams } from "react-router-dom";
 import cat from './download.jpg';
 
 function App() {
   return (
     <div className="App">
-      {/* <Load /> */}
+      <Load />
       <BrowserRouter>
         <Routes>
           {/* <Route path={'/:id'} element={<Detail />} /> */}
-          <Route path="/" element={<A />} />
+          {/* <Route path="/" element={<A />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
-
-function A() { // eslint-disable-next-line
+// eslint-disable-next-line
+function A() {
   const [pr, setPr] = useSearchParams();
   const [text, setText] = useState('');
   return <div className="A">
@@ -38,6 +38,10 @@ function Detail() {
 function Load() {
   const [list, setList] = useState([]);
   const [word, serWord] = useState('');
+  const [postlist, setPostist] = useState({ bookid: '', bookname: '', publisher: '', price: '' });
+  useEffect(e => {
+    setPostist({ bookid: '', bookname: '', publisher: '', price: '' });
+  }, []);
   return (
     <div className="load">
       {/* {list && list.map((i, n) => <div key={n}>
@@ -53,6 +57,21 @@ function Load() {
           // console.log(e.data);
         })
       }}>load</button>
+      <div>
+        <input onChange={e => setPostist(a => [{ ...a, bookid: e.target.value }])} value={postlist.bookid} placeholder="id" />
+        <input onChange={e => setPostist(a => [{ ...a, bookname: e.target.value }])} value={postlist.bookname} placeholder="bookname" />
+        <input onChange={e => setPostist(a => [{ ...a, publisher: e.target.value }])} value={postlist.publisher} placeholder="publisher" />
+        <input onChange={e => setPostist(a => [{ ...a, price: e.target.value }])} value={postlist.price} placeholder="price" />
+        <button onClick={async e => {
+          await axios.post('http://localhost:3333/insert', {
+            id: postlist.bookid, name: postlist.bookname, publisher: postlist.publisher, price: postlist.price
+          }).then(e => {
+            console.log(e);
+          }).catch(e => {
+            console.log(e);
+          });
+        }}>삽입</button>
+      </div>
     </div>
   );
 }
