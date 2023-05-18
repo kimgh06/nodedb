f3e10776836216b2f793c869107f961a33b9a206
kkh061101 <kkh061101@naver.com>
2023-05-18 11:54:53 +0900

    스타일 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { BrowserRouter, Link, Route, Routes, useParams } from "react-router-dom";
 import Showdown from "showdown";
+import "./App.scss";
 
 function App() {
   return (
@@ -10,10 +11,44 @@ function App() {
         <Routes>
           <Route path={'/:id'} element={<Detail />} />
           <Route path="/" element={<Load />} />
+          <Route path={'/modify/:id'} element={<Modify />} />
         </Routes>
       </BrowserRouter>
     </div>
   );
+}
+
+function Modify() {
+  const { id } = useParams();
+  const [toupdate, setToupdate] = useState({ title: '', maintext: '' });
+  useEffect(e => {
+    find();
+    //eslint-disable-next-line
+  }, [])
+  const find = async e => {
+    await axios.post('http://localhost:3333/find', { id: id }).then(e => {
+      setToupdate(a => ({ ...a, title: e.data[0]?.title, maintext: e.data[0]?.maintext }));
+    })
+  }
+  return <div className="modify">
+    <div>
+      <input onChange={e => setToupdate(a => ({ ...a, id: e.target.value }))} value={id} placeholder="id" type={'number'} readOnly />
+      <br />
+      <input onChange={e => setToupdate(a => ({ ...a, title: e.target.value }))} value={toupdate?.title} placeholder="title" type={'text'} /><br />
+      <textarea onChange={e => setToupdate(a => ({ ...a, maintext: e.target.value }))} value={toupdate?.maintext} placeholder="markdown text" type={'text'} />
+      <button onClick={async e => {
+        await axios.post('http://localhost:3333/update', {
+          id: id, name: escapeText(toupdate.title), maintext: escapeText(toupdate.maintext)
+        }).then(e => {
+          if (e.status === 201) {
+            window.location.href = `/${id}`;
+          }
+        }).catch(e => {
+          console.log(e);
+        });
+      }}>수정</button>
+    </div>
+  </div>;
 }
 // eslint-disable-next-line
 function Detail() {
@@ -24,16 +59,12 @@ function Detail() {
     await axios.post('http://localhost:3333/find', { id: id })
       .then(e => {
         let datas = e.data[0];
-        for (let i in datas) {
-          if (datas.date !== null) {
-            datas.date = datas.date.slice(0, 10);
-          }
+        if (datas.date !== null) {
+          datas.date = datas.date.slice(0, 10);
         }
         setList(datas);
       }).catch(e => {
-        // if (e.response.status === 404) {
-        setList({ id: 'NOT FOUND', title: 'NOT FOUND', maintext: '# NOT FOUND' });
-        // }
+        setList({ id: 'NOT FOUND', title: 'NOT FOUND', maintext: '## NOT FOUND' });
       });
   }
   useEffect(e => {
@@ -41,6 +72,7 @@ function Detail() {
     //eslint-disable-next-line
   }, []);
   return <div className="detail">
+    <Link to={'/'}><h1>◀</h1></Link>
     <h1>{list?.title} {list?.date}</h1>
     <button onClick={async e => {
       if (window.confirm("삭제할 건가?")) {
@@ -53,19 +85,30 @@ function Detail() {
         window.location.href = '/';
       }
     }}>게시물 삭제하기</button>
+    <Link to={`/modify/${id}`}>
+      <button>
+        게시물 수정하기
+      </button>
+    </Link>
     <div dangerouslySetInnerHTML={{ __html: converter.makeHtml(list?.maintext) }}></div>
   </div>;
+}
+
+const escapeText = text => {
+  let returntext;
+  returntext = text.replace(/'/g, "\\'");
+  returntext = returntext.replace(/"/g, '\\"');
+  returntext = returntext.replace(/`/g, '\\`');
+  return returntext;
 }
 
 // eslint-disable-next-line
 function Load() {
   const [list, setList] = useState([]);
-  const [word, serWord] = useState('');
   const [postlist, setPostist] = useState({ id: '', title: '', maintext: '' });
-  const [toupdate, setToupdate] = useState({ id: '', title: '', maintext: '' });
 
   const initsetting = async e => {
-    await axios.post('http://localhost:3333/loadall', { id: word }).then(e => {
+    await axios.post('http://localhost:3333/loadall').then(e => {
       let datas = e.data;
       for (let i in datas) {
         if (datas[i].date !== null) {
@@ -74,27 +117,22 @@ function Load() {
       }
       setList(datas);
     });
+  }
+  const selectmax = async e => {
     await axios.post('http://localhost:3333/selectmax').then(e => {
       setPostist(a => ({ ...a, id: e.data[0].id }))
     });
   }
 
   useEffect(e => {
+    initsetting();
+    selectmax();
     setPostist({ id: '', title: '', maintext: '' });
-    setToupdate({ id: ``, title: '', maintext: '' });
     setInterval(() => {
       initsetting();
-    }, 500);
+    }, 1000);
     //eslint-disable-next-line
   }, []);
-
-  const escapeText = text => {
-    let returntext;
-    returntext = text.replace(/'/g, "\\'");
-    returntext = returntext.replace(/"/g, '\\"');
-    returntext = returntext.replace(/`/g, '\\`');
-    return returntext;
-  }
 
   return (
     <div className="load">
@@ -112,20 +150,15 @@ function Load() {
           </tr>)}
         </tbody>
       </table>
-      <input value={word} onChange={e => serWord(v => e.target.value)} />
-      <button onClick={async function (e) {
-        await axios.post('http://localhost:3333/find', { id: word }).then(e => {
-          setList(e.data);
-        })
-      }}>load</button>
       <div>
-        <input onChange={e => setPostist(a => ({ ...a, id: e.target.value }))} value={postlist.id} placeholder="id" type={'number'} readOnly={true} />
+        <input onChange={e => setPostist(a => ({ ...a, id: e.target.value }))} value={postlist.id} placeholder="id" type={'number'} readOnly={false} />
+        <br />
         <input onChange={e => setPostist(a => ({ ...a, title: e.target.value }))} value={postlist.title} placeholder="title" type={'text'} />
         <br />
-        <textarea onChange={e => setPostist(a => ({ ...a, maintext: e.target.value }))} value={postlist.maintext} placeholder="maintext" type={'text'} />
+        <textarea onChange={e => setPostist(a => ({ ...a, maintext: e.target.value }))} value={postlist.maintext} placeholder="markdown text" type={'text'} />
         <button onClick={async e => {
           await axios.post('http://localhost:3333/insert', {
-            id: postlist.id, name: postlist.title, maintext: escapeText(postlist.maintext), date: new Date().toLocaleDateString().replace(/. /g, '-')
+            id: postlist.id, name: escapeText(postlist.title), maintext: escapeText(postlist.maintext), date: new Date().toLocaleDateString().replace(/. /g, '-')
           }).then(e => {
             if (e.status === 201) {
               console.log('성공적으로 생성했습니다!');
@@ -134,26 +167,6 @@ function Load() {
             console.log(e);
           });
         }}>삽입</button>
-      </div>
-      <div>
-        <input onChange={e => setToupdate(a => ({ ...a, id: e.target.value }))} value={toupdate.id} placeholder="id" type={'number'} />
-        <button onClick={async function (e) {
-          await axios.post('http://localhost:3333/find', { id: toupdate.id }).then(e => {
-            setToupdate(a => ({ ...a, title: e.data[0]?.title, maintext: e.data[0]?.maintext }));
-          })
-        }}>수정목록 가져오기</button>
-        <br />
-        <input onChange={e => setToupdate(a => ({ ...a, title: e.target.value }))} value={toupdate.title} placeholder="title" type={'text'} /><br />
-        <textarea onChange={e => setToupdate(a => ({ ...a, maintext: e.target.value }))} value={toupdate.maintext} placeholder="maintext" type={'text'} />
-        <button onClick={async e => {
-          await axios.post('http://localhost:3333/update', {
-            id: toupdate.id, name: toupdate.title, maintext: escapeText(toupdate.maintext)
-          }).then(e => {
-            console.log(e.status);
-          }).catch(e => {
-            console.log(e);
-          });
-        }}>수정</button>
       </div>
     </div>
   );
